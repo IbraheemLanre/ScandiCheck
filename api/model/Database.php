83560ca46a265ec52d38d5449b816909dcb92c59
IbraheemLanre <s2001108@edu.bc.fi>
2021-11-04 16:20:48 +0200

    initial commit for check

---
+++ api/model/Database.php
@@ -0,0 +1,24 @@
+<?php
+class Database{
+    protected $connection = null;
+    private $DB_HOST = "localhost";
+    private $DB_USERNAME = "root";
+    private $DB_PASSWORD = "";
+    private $DB_NAME = "productdb";
+
+    public function getDB(){
+        try{
+            $this->connection = new mysqli($this->DB_HOST, $this->DB_USERNAME, $this->DB_PASSWORD, $this->DB_NAME);
+
+            if(mysqli_connect_errno()){
+                throw new Exception("Could not connect to database.");
+            }
+        }catch(\Exception $e){
+            throw new Exception($e->getMessage());
+        }
+
+        return $this->connection;
+    }
+
+ }
+?>
